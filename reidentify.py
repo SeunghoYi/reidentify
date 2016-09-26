a60e16f3b604f0546fe0fa768a02fb127a87a6f7
cosine0 <ksitht@gmail.com>
2016-09-27 02:09:35 +0900

    complete for exact matching

--- reidentify.py
+++ reidentify.py
@@ -1,14 +1,12 @@
-# coding=utf-8
 # I assume all original data is list of dict
-REQUIRED_INFORMATION = ['name', 'birthday', 'address', 'sex']
-
-# input collected data
-data_tables = input()
-sensitive_medical_table = input()
+import sqlite3
+import csv
+from collections import defaultdict
 
 
 class DeidentifiedContent(object):
     def __init__(self, method, content):
+        super().__init__()
         assert isinstance(method, str)
         assert isinstance(content, str)
         self.method = 'masking'
@@ -25,11 +23,25 @@ class MaskedContent(DeidentifiedContent):
 
 
 def mergeable(to_content, from_content):
-    return to_content == from_content
+    if isinstance(to_content, (str, bytes)) and isinstance(from_content, (str, bytes)):
+        return to_content == from_content
+    elif isinstance(to_content, (list, tuple, set)) and isinstance(from_content, (str, bytes)):
+        return from_content in to_content
+    elif isinstance(to_content, (str, bytes)) and isinstance(from_content, (list, tuple, set)):
+        return to_content in from_content
+    elif isinstance(to_content, (list, tuple, set)) and isinstance(from_content, (list, tuple, set)):
+        return not set(to_content).isdisjoint(from_content)
 
 
 def merge(content1, content2):
-    return content1
+    if isinstance(content1, (str, bytes)) and isinstance(content2, (str, bytes)):
+        return content1
+    elif isinstance(content1, (list, tuple, set)) and isinstance(content2, (str, bytes)):
+        return set(content2) | {content1}
+    elif isinstance(content1, (str, bytes)) and isinstance(content2, (list, tuple, set)):
+        return set(content1) | {content2}
+    elif isinstance(content1, (list, tuple, set)) and isinstance(content2, (list, tuple, set)):
+        return not set(content1) | set(content2)
 
 
 def join(total_data, additional_data):
@@ -68,7 +80,7 @@ def join(total_data, additional_data):
                 # mark as already matched to perform outer join
                 matching_total_data_row[('has matched',)] = True
                 joined_row = matching_total_data_row[:]
-                for column_name, content in additional_table_row.iteritems():
+                for column_name, content in additional_table_row.items():
                     if column_name not in matching_total_data_row:
                         joined_row[column_name] = content
                 result_set.append(joined_row)
@@ -89,27 +101,58 @@ def join(total_data, additional_data):
     return result_set
 
 
+def get_dataset_from_sqlite_narrow_table(file_name, table_name, id_column='id', key_column='key', value_column='value'):
+    cursor = sqlite3.connect(file_name).cursor()
+    cursor.execute('SELECT {id}, {key}, {value} FROM {table}'.format(id=id_column, key=key_column, value=value_column,
+                                                                     table=table_name))
+    data_set = defaultdict(lambda: defaultdict(set))
+    for row_id, key, value in cursor.fetchall():
+        data_set[row_id][key].add(value)
+
+    return data_set.values()
+
+
+def get_dataset_from_csv(file_name):
+    with open(file_name, encoding='utf-8') as f:
+        data_set = list(csv.DictReader(f))
+    return data_set
+
+
+def find(data_set, query_dict):
+    result_set = []
+    for data_row in data_set:
+        for column_name, value in query_dict.items():
+            if column_name not in data_row:
+                break
+
+            if not mergeable(data_row[column_name], value):
+                break
+        else:
+            result_set.append(data_row)
+
+    return result_set
+
+
 def main():
     # cross
-    total_data = sensitive_medical_table
+    sensitive_medical_table = get_dataset_from_csv('bob_medical.csv')
 
-    for table in data_tables:
-        total_data = join(total_data, table)
-        if all(field in total_data.fileds for field in REQUIRED_INFORMATION):
-            break
+    facebook_data = get_dataset_from_sqlite_narrow_table('facebook.db', 'fb', 'url', 'key', 'value')
 
+    total_data = join(sensitive_medical_table, facebook_data)
+    print(total_data)
     # search
-    hyeon_row = total_data.find({'name': '현범수', 'birthday': '1.1', 'address': 'jeju-do jeju-si 1-1', 'sex': '남'})
-    print hyeon_row['illness']
+    hyeon_rows = find(total_data, {'이메일': 'naver.com'})
+    print(hyeon_rows)
 
     #
     # - or -
     #
 
-    # print all
-    unique_persons = unique(total_data, where=('name', 'birthday', 'address', 'sex'))
-    for row in unique_persons:
-        print row['name'], row['birthday'], row['address'], row['sex'], row['illness']
+    # # print all
+    # unique_persons = unique(total_data, where=('name', 'birthday', 'address', 'sex'))
+    # for row in unique_persons:
+    #     print(row['name'], row['birthday'], row['address'], row['sex'], row['illness'])
 
 
 if __name__ == '__main__':
